ccc56146a54f574a70334e4cb203492827536fad
Gzoe <847952879@qq.com>
2022-04-14 16:14:16 +1200

    End of Section5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
